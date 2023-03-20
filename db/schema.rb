10b12f5691c7b86199b0869658fe6cebcde937ae
daniel <daniel.oliveira.au@hotmail.com>
2023-03-20 16:58:36 -0300

    fix: model conflict

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_193422) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "fist_name"
-    t.string "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
