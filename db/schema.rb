1a5c17386fe344acd65c5eeed84dbb59ec262d62
daniel <daniel.oliveira.au@hotmail.com>
2023-03-20 17:08:12 -0300

    fix: model conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_193422) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_20_184209) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
